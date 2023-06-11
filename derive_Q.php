88ae349096eb29b39acac47a92a12eb93ea8a7d9
sushidolphin <134845495+sushidolphin@users.noreply.github.com>
2023-06-11 13:33:31 +0200

    Update derive_Q.php

--- derive_Q.php
+++ derive_Q.php
@@ -121,18 +121,25 @@ x_roe_return_on_equity_median_T28Q
 
 */
 
+// Quarter report data are being used
+
 /*
 
-create_var ( NAME, VALUE, 1 = save );
+	create_var ( NAME, VALUE, 1 = save );
 
-create_trailing_var ( NAME or null, FIELD_NAME, number of trailing values, calculation: sum, mean, percentile, SD, median or null for array, 1 = save );
+	create_trailing_var ( NAME or null, FIELD_NAME, number of trailing values, calculation: sum, mean, percentile, SD, median or null for array, 1 = save );
 
-calculate_growth ( FIELD_NAME , compare_with_X_report_ago , 1 = save );
+	calculate_growth ( FIELD_NAME , compare_with_X_report_ago , 1 = save );
 
-calculate_consistency ( FIELD_NAME , 1 for increase or -1 for decrease, number of trailing values, 1 = save );
+	calculate_consistency ( FIELD_NAME , 1 for increase or -1 for decrease, number of trailing values, 1 = save );
 
 */
 
+
+
+
+
+// CURRENT VALUES
 				
 create_var( "price_median", calculate_median( $temp[ "price_data_within_period" ][ "price_median" ] ), 1 );
 														
@@ -146,28 +153,6 @@ create_var( "PE",
 	
 , 1 );
 
-create_trailing_var ( "eps_TTM", "eps_earnings_per_share", 4, "sum", 1 );
-
-create_trailing_var ( "net_income_TTM", "net_income", 4, "sum", 1 );
-
-create_trailing_var ( "net_cash_flow_TTM", "net_cash_flow", 4, "sum", 1 );
-
-create_trailing_var ( "share_holder_equity_TTM", "share_holder_equity", 4, "mean", 1 );
-
-create_trailing_var ( null, "PE", 20, "90th", 1 );
-
-create_trailing_var ( null, "PE", 20, "10th", 1 );
-
-create_trailing_var ( null, "PE", 20, "SD", 1 );
-
-create_trailing_var ( null, "roe_return_on_equity", 20, "median", 1 );
-
-create_trailing_var ( null, "roe_return_on_equity", 28, "median", 1 );
-
-create_trailing_var ( null, "net_profit_margin", 20, "median", 1 );
-
-create_trailing_var ( null, "net_profit_margin", 28, "median", 1 );
-
 create_var( "years_to_repay_LT_debt", 
 
 	( get_financial( 0, "net_income_TTM" ) > 0 ) 
@@ -178,17 +163,74 @@ create_var( "years_to_repay_LT_debt",
 	
 , 1 );
 
-calculate_consistency ( "net_income_TTM", 1, 28, 1 );
+create_var ( "EPS_TTM_growth_greater_than_net_income_TTM_growth",
 
-calculate_growth ( "net_income_TTM", 20, 1 );
-
-create_var ( "shares_buyback",
-
-	( get_financial( -1, "shares_outstanding" ) > get_financial( 0, "shares_outstanding" ) )
+	( get_financial( 0, "eps_TTM_annualized_growth_T20Q" ) > get_financial( 0, "net_income_TTM_annualized_growth_T20Q" ) )
 
 	? 1 : 0,
 
 	1 
 );
+
+
+
+
+
+// TTM VALUES
+
+create_trailing_var ( "eps_TTM", "eps_earnings_per_share", 4, "sum", 1 );
+
+create_trailing_var ( "net_income_TTM", "net_income", 4, "sum", 1 );
+
+create_trailing_var ( "net_cash_flow_TTM", "net_cash_flow", 4, "sum", 1 );
+
+create_trailing_var ( "share_holder_equity_TTM", "share_holder_equity", 4, "mean", 1 );
+
+create_trailing_var ( null, "PE", 20, "75th", 1 );
+
+create_trailing_var ( null, "PE", 20, "25th", 1 );
+
+create_trailing_var ( null, "PE", 20, "SD", 1 );
+
+
+
+
+
+create_var ( "shares_buyback_with_high_PE",
+
+	( get_financial( -1, "shares_outstanding" ) > get_financial( 0, "shares_outstanding" ) AND get_financial( 0, "PE" ) >= get_financial( 0, "PE_75th_T20Q" ) )
+
+	? 1 
+	
+	: 0,
+
+	1 
+);
+
+
+
+
+
+// 5 AND 7 YRS VALUES
+
+foreach ( [ 20, 28 ] as $time_window )
+{
+
+	create_trailing_var ( null, "roe_return_on_equity", $time_window, "median", 1 );
+
+	create_trailing_var ( null, "net_profit_margin", $time_window, "median", 1 );
+
+
+	calculate_consistency ( "net_income_TTM", 1, $time_window, 1 );
+
+	calculate_consistency ( "eps_TTM", 1, $time_window, 1 );
+
+
+	calculate_growth ( "net_income_TTM", $time_window, 1 );
+
+	calculate_growth ( "eps_TTM", $time_window, 1 );
+
+}
+
 					
 ?>
