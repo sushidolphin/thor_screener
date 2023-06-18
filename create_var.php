7726ca345b0cc5c5b9769dc9670d152c36ca6e90
sushidolphin <134845495+sushidolphin@users.noreply.github.com>
2023-06-18 18:20:19 +0200

    Add files via upload

--- create_var.php
+++ create_var.php
@@ -2,33 +2,40 @@
 
 function create_var ( $name, $value, $save )
 {
-	global $settings, $financial_report, $derived, $save_to_database, $derived_fields_prefix;
+	global $temp, $settings, $financial_report, $derived, $save_to_database, $derived_fields_prefix, $fiscal_report_absolute_position;
 	
-	if 
-	( 
-		!isset( $financial_report[ $derived_fields_prefix . $name ] ) 
-		
-		OR 
-		
+	if ( is_numeric( $value ) AND !is_nan( $value ) )
+	{
+		if 
 		( 
+			!isset( $financial_report[ $derived_fields_prefix . $name ] ) 
 			
-			$settings[ "recalculate_derived_vars" ] == 1			
-			
-			OR
+			OR 
 			
 			( 
-				is_array( $settings[ "recalculate_derived_vars" ] ) 
 				
-				AND 
+				$settings[ "recalculate_derived_vars" ] == 1			
 				
-				match_name_to_column( $name , $settings[ "recalculate_derived_vars" ] ) != null
-			)
-											
-		)		
+				OR
+				
+				( 
+					is_array( $settings[ "recalculate_derived_vars" ] ) 
+					
+					AND 
+					
+					match_name_to_column( $name , $settings[ "recalculate_derived_vars" ] ) != null
+				)
+												
+			)		
+			
+		) 
+		// $financial_report[ $derived_fields_prefix . $name ] = $value;
 		
-	) $financial_report[ $derived_fields_prefix . $name ] = $value;
-	
-	if ( $save == 1 AND ( !isset( $save_to_database ) OR !in_array( $derived_fields_prefix . $name, $save_to_database ) ) ) $save_to_database[] = $derived_fields_prefix . $name;
+		$temp[ "previous_financial_reports" ][ $fiscal_report_absolute_position ][ $derived_fields_prefix . $name ] = $value;
+		
+		if ( $save == 1 AND ( !isset( $save_to_database ) OR !in_array( $derived_fields_prefix . $name, $save_to_database ) ) ) $save_to_database[] = $derived_fields_prefix . $name;
+	}
+	else return null;
 }
 
 
